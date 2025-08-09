1875811fd7b8403daf5b866f59c670423c8fe64f
SofiaParodi <sofia.parodi503@gmail.com>
2025-08-09 18:23:19 -0300

    se renderiza dinamicamente el listado de tareas, funcionalidad del formulario y funcionalidad en el boton limpiar lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,59 @@
 /* variable declaration */
-
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 const addBtn = $('#addBtn');
 const formSection = $('#formSection');
 const mainSection = $('#mainSection');
 const cancelBtn = $('#cancelBtn');
+const submitBtn = $('#submitBtn');
+const titleInput = $('#titleInput');
+const categoriesSelect = $('#categoriesSelect');
+const taskList = $('#taskList');
+const clearListBtn = $('#clearListBtn');
 
+/* functions */
 
-/* function */
+function renderList () {
+    taskList.innerHTML = '';
+
+    if (tasks.length === 0) {
+        const emptyLi = document.createElement('li');
+        emptyLi.classList.add('empty-list');
+        emptyLi.textContent = '¡todas tus tareas están completadas!';
+        taskList.appendChild(emptyLi);
+        return;
+    }
+
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.classList.add('row');
+        li.classList.add('list-item')
+        li.innerHTML = `
+            <h3 class="title">${getEmoji(task.category)} ${task.title}</h3>
+                <input type="checkbox">
+        `
+        taskList.appendChild(li);
+    });
+}
+
+function getEmoji (category) {
+    switch (category) {
+         case 'work':
+            return '💼';
+        case 'study':
+            return '📚';
+        case 'personal':
+            return '🏠';
+    }
+}
+
 function $ (selector) {
     return document.querySelector(selector);
 }
 
+renderList();
+
+/* events*/
 addBtn.addEventListener('click', () => {
     formSection.classList.toggle('hidden');
     mainSection.classList.toggle('hidden');
@@ -20,4 +62,30 @@ addBtn.addEventListener('click', () => {
 cancelBtn.addEventListener('click', () => {
     formSection.classList.toggle('hidden');
     mainSection.classList.toggle('hidden');
+})
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const title = titleInput.value.trim();
+    const category = categoriesSelect.value;
+
+    if (!title || !category) {
+        alert('completa todos los campos');
+        return;
+    }
+
+    tasks.push({ title, category });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderList();
+
+    titleInput.value = '';
+    categoriesSelect.value = '';
+    formSection.classList.add('hidden');
+    mainSection.classList.remove('hidden');
+});
+
+clearListBtn.addEventListener('click', () => {
+    tasks.length = 0;
+    localStorage.removeItem('tasks');
+    renderList();
 })
